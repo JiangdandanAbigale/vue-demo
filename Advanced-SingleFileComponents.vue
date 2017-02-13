bcbbc05a61207e7be2834fd809ebb11f2175cc4b
Abigale <609262982@qq.com>
2017-02-13 10:08:24 +0800

    进阶-routing

---
+++ Advanced-SingleFileComponents.vue
@@ -0,0 +1,18 @@
+<template>
+	<p>{{greeting}} world!</p>
+</template>
+<script>
+	module.exports = {
+		data(){
+			return{
+				greeting:'Hello',
+			}
+		}
+	}
+</script>
+<style scoped>
+	p{
+		font-size:20px;
+		text-align: center;
+	}
+</style>
